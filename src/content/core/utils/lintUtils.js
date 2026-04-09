724a8697a80134154b5e4469b6219f7930260aa7
Bogdan Sandu <bogdanelsandu2011@gmail.com>
2026-04-09 20:21:36 +0300

    lintutils: add jsdoc warnings

--- src/content/core/utils/lintUtils.js
+++ src/content/core/utils/lintUtils.js
@@ -1,6 +1,12 @@
 /**
+ * Used to get rid of linting errors.
+ * 
+ * When calling __unused(x), the variable `x` becomes unusable (an error will be thrown during building)
+ * 
+ * WARNING: DO NOT USE IN BRANCHING LOGIC
+ * 
  * @param {T} v
- * @returns {T} 
+ * @returns {undefined} 
  */
 export default function __unused(v) {
     v;
