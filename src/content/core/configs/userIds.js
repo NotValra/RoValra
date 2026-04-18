eb4eb7bcfc24c62d077da73506ce4839fdfdbb25
noahbassett <98113336+lolct@users.noreply.github.com>
2026-04-18 08:14:20 +0200

    feat: legacy theme switcher

--- src/content/core/configs/userIds.js
+++ src/content/core/configs/userIds.js
@@ -11,7 +11,8 @@ export const CONTRIBUTOR_USER_IDS = [
     '126448532', //steinann
     '1564574922', //cornusandu
     '587159802', //zoinbase
-    '193520242', // tigodev1
+    '193520242', //tigodev1
+    '2615068449', //lolct
 ];
 
 export const TESTER_USER_IDS = [
