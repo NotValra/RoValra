60129957d92472b627c6f3adb0db9d72eac70aa6
Bogdan Sandu <bogdanelsandu2011@gmail.com>
2026-05-20 10:49:03 +0300

    cleanup

--- src/content/core/settings/settingsCompat.ts
+++ src/content/core/settings/settingsCompat.ts
@@ -1,5 +1,5 @@
-import Version, { getVersion } from "../utils/version.js";
 import { SETTINGS_CONFIG } from "./settingConfig.js";
+import { loadSettings } from "./handlesettings.js";
 
 const settingDeprecations: Record<string, ((value: any, gets: (key: string) => Promise<any>, sets: (key: string, value: any) => void) => void) | undefined> = {
     "EnableGameTrailer": undefined,
@@ -9,76 +9,67 @@ const settingDeprecations: Record<string, ((value: any, gets: (key: string) => P
 
 const initPromise = (async () => {
     console.log("RoValra: Verifying settings compat.");
-    let oldVersion;
-    try {
-        oldVersion = (await chrome.storage.local.get("RoValraSettingsVersion")).RoValraSettingsVersion;
-        if (oldVersion === undefined)
-            throw Error("yes()");
-    } catch {
-        oldVersion = "2.5.2";
+    const settings = await loadSettings();
+        
+    let deleted = [];
+    let replaced = [];
+    for (const [setting, replaceFn] of Object.entries(settingDeprecations)) {
+        try {
+            let v: any = undefined;
+            if ((v = (await chrome.storage.local.get({[setting]: undefined}))[setting]) !== undefined) {
+                if (replaceFn === undefined) {
+                    console.info(`Deleted setting: ${setting}. No suitable replacement.`);
+                    deleted.push(setting);
+                    await chrome.storage.local.remove(setting);
+                } else {
+                    console.info(`Updating setting ${setting}.`, `(with function: \`${replaceFn.toString()}\`)`);
+                    try {
+                        const replacements: Record<string, any> = {};
+                        replaceFn(
+                            v,
+                            async (key) => (await chrome.storage.local.get({[key]: undefined}))[key],
+                            (key, newValue) => {replacements[key] = newValue;}
+                        );
+                        await chrome.storage.local.set(replacements);
+                        replaced.push(setting);
+                    } catch (e) {
+                        console.error(`Failed to update setting ${setting} — unexpected error: `, e);
+                    }
+                }
+            }
+        } catch (e) {
+            console.error(`Failed to retrieve setting ${setting} for compat checks — unexpected error: `, e);
+        }
     }
-    const oldv = new Version(oldVersion);
-    const newv = getVersion();
-    if (true) {
-
-        let deleted = [];
-        let replaced = [];
-        for (const [setting, replaceFn] of Object.entries(settingDeprecations)) {
-            try {
-                let v: any = undefined;
-                if ((v = (await chrome.storage.local.get({[setting]: undefined}))[setting]) !== undefined) {
-                    if (replaceFn === undefined) {
-                        console.info(`Deleted setting: ${setting}. No suitable replacement.`);
-                        deleted.push(setting);
-                        await chrome.storage.local.remove(setting);
-                    } else {
-                        console.info(`Updating setting ${setting}.`, `(with function: \`${replaceFn.toString()}\`)`);
-                        try {
-                            const replacements: Record<string, any> = {};
-                            replaceFn(
-                                v,
-                                async (key) => (await chrome.storage.local.get({[key]: undefined}))[key],
-                                (key, newValue) => {replacements[key] = newValue;}
-                            );
-                            await chrome.storage.local.set(replacements);
-                            replaced.push(setting);
-                        } catch (e) {
-                            console.error(`Failed to update setting ${setting} — unexpected error: `, e);
-                        }
-                    }
-                }
-            } catch (e) {
-                console.error(`Failed to retrieve setting ${setting} for compat checks — unexpected error: `, e);
-            }
-        }
-        const forEachLockedSetting = (key: string, data: Record<string, any>) => {
-            const name = data.label;
-            deleted.push(key);
-        };
-        for (const [category, settings] of Object.entries(SETTINGS_CONFIG)) {
-            for (const [setting, data] of Object.entries(settings.settings)) {
-                if (data['locked'] !== undefined || data['deprecated'] !== undefined) {
-                    let value = (await chrome.storage.local.get({[setting]: undefined}))[setting]
-                    if (value !== undefined && value !== false) {
-                        forEachLockedSetting(setting, data);
-                        await chrome.storage.local.remove(setting);
-                    }
+    const forEachLockedSetting = (key: string, data: Record<string, any>) => {
+        const name = data.label;
+        deleted.push(name);
+    };
+    for (const [category, settings] of Object.entries(SETTINGS_CONFIG)) {
+        for (const [setting, data] of Object.entries(settings.settings)) {
+            if (data['locked'] !== undefined || data['deprecated'] !== undefined) {
+                let value = (await chrome.storage.local.get({[setting]: undefined}))[setting]
+                if (value !== undefined && value !== false) {
+                    forEachLockedSetting(setting, data);
+                    await chrome.storage.local.remove(setting);
                 }
             }
         }
+    }
 
-        if (replaced.length >= 1) {
-            alert(`(RoValra) The following settings have been recently replaced or changed:
+    if (replaced.length >= 1) {
+        alert(`(RoValra) The following settings have been recently replaced or changed:
     *  ${replaced.join("\n\t*  ")}`);
-        }
-        if (deleted.length >= 1) {
-            alert(`(RoValra) The following settings have been recently deleted, locked or deprecated:
-    *  ${deleted.join("\n\t*  ")}`);
-        }
-
-        await chrome.storage.local.set({"RoValraSettingsVersion": chrome.runtime.getManifest().version});
-
     }
+
+    if (deleted.length >= 1) {
+        alert(`(RoValra) The following settings have been recently deleted, locked or deprecated:
+    *  ${deleted.join("\n    *  ")}`);
+    }
+
+    await chrome.storage.local.set({"RoValraSettingsVersion": chrome.runtime.getManifest().version});
+
+    console.info("Setting compat checks finished.");
 })();
 
 export default initPromise;
