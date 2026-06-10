e9aa398de51e0559bf91a8dd38fe912b9d3f308f
Bogdan Sandu <bogdanelsandu2011@gmail.com>
2026-06-10 16:31:06 +0300

    [fix] remove lessPlus dependency on collapsible sidebar

--- src/content/features/sitewide/lessPlus.js
+++ src/content/features/sitewide/lessPlus.js
@@ -1,4 +1,5 @@
 import { settings } from "../../core/settings/getSettings";
+import { debugVerbose } from "../../core/debug";
 
 const plusTypeEnum = Object.freeze({
     Full: 0,
@@ -15,34 +16,38 @@ async function asyncInit() {
     else plusType = plusTypeEnum.Full;
 
     const hook = (warnOnNotFound) => {
+        debugVerbose(`[lessPlus] hook() running`, {'warnOnNotFound': warnOnNotFound, 'plusTypeStateEnum': Object.keys(plusTypeEnum).find((k) => plusTypeEnum[k] === plusType), 'plusTypeState': plusType});
         if (plusType >= plusTypeEnum.Reduced) {
             const navbar = document.querySelector("#left-navigation-container .left-nav div");
-            const robloxPlus = navbar.querySelectorAll("a[data-rovalra-sidebar-tooltip-text='Roblox Plus']");
+            const robloxPlus = navbar.querySelectorAll("li:not(.padding-top-xsmall) a[href$='/plus']");
 
-            if (robloxPlus[0])
+            if (robloxPlus[0]) {
+                debugVerbose(`[lessPlus] Removing robloxPlus button from sidebar.`, {'element': robloxPlus});
                 robloxPlus[0].parentElement.remove();
-            else if (warnOnNotFound)
+            } else if (warnOnNotFound)
                 console.error("robloxPlus not found.");
 
             const _RobloxPlusNoteA = document.querySelectorAll(
-                "#left-navigation-container .left-nav div li.padding-top-xsmall a[href='/plus']",
+                "#left-navigation-container .left-nav div li.padding-top-xsmall a[href='/plus']:not(.minimised-robloxplus-note)",
             );
             const robloxPlusNote = _RobloxPlusNoteA[0];
             if (plusType >= plusTypeEnum.None) {
-                if (robloxPlusNote?.parentElement)
+                if (robloxPlusNote?.parentElement) {
+                    debugVerbose(`[lessPlus] Removing robloxPlus note from sidebar.`, {'element': robloxPlusNote});
                     robloxPlusNote.parentElement.remove();
-                else if (warnOnNotFound)
+                } else if (warnOnNotFound)
                     console.error("robloxPlusNote.parentElement not found (no plus).");
             } else {
-                if (robloxPlusNote?.parentElement)
+                if (robloxPlusNote?.parentElement) {
+                    debugVerbose(`[lessPlus] Minimizing robloxPlus note from sidebar.`, {'element': robloxPlusNote});
                     robloxPlusNote.parentElement.innerHTML = String.raw`
                         <p class="text-body-medium padding-x-medium padding-y-small" style="white-space: nowrap;">
                           <span role="presentation" class="grow-0 shrink-0 basis-auto icon icon-regular-roblox-plus size-[var(--icon-size-small)]" style="vertical-align: -1px;"></span>
                           More fun for less Robux. 
-                          <a href='/plus' class="content-default [text-decoration:underline] [text-decoration-skip-ink:none] [text-underline-offset:3px]">Subscribe</a>
+                          <a href='/plus' class="content-default minimised-robloxplus-note [text-decoration:underline] [text-decoration-skip-ink:none] [text-underline-offset:3px]">Subscribe</a>
                         </p>
                     `; // Verified
-                else if (warnOnNotFound)
+                } else if (warnOnNotFound)
                     console.error("robloxPlusNote.parentElement not found (less plus).");
             }
 
@@ -56,8 +61,8 @@ async function asyncInit() {
                         .parentElement.children[1];
 
                 if (plusType >= plusTypeEnum.None)
-                    robloxPlusInBuyRobuxSnippet.parentElement.remove();
-                else robloxPlusInBuyRobuxSnippet.remove();
+                    {robloxPlusInBuyRobuxSnippet.parentElement.remove(); debugVerbose(`[lessPlus] Minimizing Roblox Plus section from Buy Robux page.`, {'element': robloxPlusInBuyRobuxSnippet});}
+                else {robloxPlusInBuyRobuxSnippet.remove(); debugVerbose(`[lessPlus] Removing Roblox Plus section from Buy Robux page.`, {'element': robloxPlusInBuyRobuxSnippet});}
             }
         }
     };
