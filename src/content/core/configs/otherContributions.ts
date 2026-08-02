c172a5b8ce2907b9db9991d6acd7e57fd87c5836
Bogdan Sandu <github.34373732@gmail.com>
2026-08-02 21:27:25 +0300

    feat(features:settings): permit viewing exactly which features someone contributed to
    Made the  clickable, with clicking it displaying each feature the user contributed to.

--- src/content/core/configs/otherContributions.ts
+++ src/content/core/configs/otherContributions.ts
@@ -12,49 +12,58 @@ class Contribution {
 
 type ContributionsType = {
     [featureName: string]: {
+        label: string,
         contributors: Contribution[]
     }
 };
 
 export const OTHER_CONTRIBUTIONS: ContributionsType = {
     Badges: {
+        label: "Badges",
         contributors: [
             new Contribution(1564574922, "badges.translatorAndNate", "https://github.com/NotValra/RoValra/pull/48"),  // @BossBoss2021
             new Contribution(650766686, "badges.nateGilbertFix", "https://github.com/NotValra/RoValra/pull/126")  // @auggeeo
         ]
     },
     Caching: {
+        label: "Caching System",
         contributors: [
             new Contribution(1564574922, "caching.hashmap", "https://github.com/NotValra/RoValra/pull/52")  // @BossBoss2021
         ]
     },
     Sidebar: {
+        label: "Roblox Sidebar",
         contributors: [
             new Contribution(48255812, "sidebar.rblxUpdateFix", "https://github.com/NotValra/RoValra/pull/55")  // @aliceeenight
         ]
     },
     RbxmParser: {
+        label: "RoValra .rbxm Parser",
         contributors: [
             new Contribution(48255812, "rbxm.fixDesyncCframe", "https://github.com/NotValra/RoValra/pull/69")  // @aliceeenight
         ]
     },
     Markdown: {
+        label: "Markdown Parser",
         contributors: [
             new Contribution(1564574922, "markdown.untrustedMd", "https://github.com/NotValra/RoValra/pull/75") // @BossBoss2021
         ]
     },
     SettingsHandler: {
+        label: "RoValra Settings Handler",
         contributors: [
             new Contribution(1564574922, "settingsHandler.unifiedApi", "https://github.com/NotValra/RoValra/pull/85") // @BossBoss2021
         ]
     },
     ContributorList: {
+        label: "RoValra Contributor List",
         contributors: [
             new Contribution(546872490, "contributorList.creditsImprovement", "https://github.com/NotValra/RoValra/pull/117"),  // @kanibal_dev
             new Contribution(1564574922, "contributorList.nonSettingCredits")  // @BossBoss2021
         ]
     },
     DonatorTierPage: {
+        label: "RoValra Donator Tier Page",
         contributors: [
             new Contribution(650766686, "donatorTierPage.newIndicators", "https://github.com/NotValra/RoValra/pull/123")  // @auggeeo
         ]
