08f0ce7a3456acf41936c5827688bc4d8661b911
sefa <sefaardauygar@gmail.com>
2026-09-10 05:26:14 +0300

    feat: add hide private servers

--- src/content/core/settings/settingConfig.js
+++ src/content/core/settings/settingConfig.js
@@ -594,6 +594,16 @@ export const SETTINGS_CONFIG = {
                         type: 'checkbox',
                         default: false,
                     },
+                    HidePrivateServersEnabled: {
+                        label: 'Hide Private Servers',
+                        description: [
+                            'Adds an option to hide private servers from the server list.',
+                        ],
+                        type: 'checkbox',
+                        default: true,
+                        storageKey: 'hiddenFriendPrivateServers',
+                        contributors: ['476449201'],
+                    },
                 },
             },
             PrivateQuickLinkCopy: {
