2d5c867e60f2a7c27e3d91aebee4a712c4838353
NotValra <valra@rovalra.com>
2026-09-19 11:03:33 +0200

    feat: changed to on by default

--- src/content/core/settings/settingConfig.js
+++ src/content/core/settings/settingConfig.js
@@ -582,7 +582,7 @@ export const SETTINGS_CONFIG = {
                             'Only changes the private server section.',
                         ],
                         type: 'checkbox',
-                        default: false,
+                        default: true,
                         contributors: ['4632962611'],
                     },
 
